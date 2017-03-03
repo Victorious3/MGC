a79bf15367ae727898cd04426048ad0cdcc36bc8
Inari <inari_whitebear@fastmail.jp>
2017-03-03 21:41:53 +0100

    More consts! consts for everyone! And made keyboard's namespace stuff nicer

--- keyboard.cpp
+++ keyboard.cpp
@@ -1,39 +1,41 @@
 #include "stddef.h"
 #include "keyboard.h"
 
-mgc::Keyboard::Keyboard() {
-}
+namespace mgc {
+	Keyboard::Keyboard() {
+	}
 
-mgc::Keyboard::Keyboard(ini::IniFile& ini) {
-	read_config(ini);
-}
+	Keyboard::Keyboard(const ini::IniFile& ini) {
+		read_config(ini);
+	}
 
-mgc::Keyboard::~Keyboard() {
-}
+	Keyboard::~Keyboard() {
+	}
 
-void mgc::Keyboard::read_config(ini::IniFile& ini) {
-	actionmap[TOGGLE_FULLSCREEN].scancodes.clear();
+	void Keyboard::read_config(const ini::IniFile& ini) {
+		actionmap[ACTIONS::TOGGLE_FULLSCREEN].scancodes.clear();
 
-	actionmap[TOGGLE_FULLSCREEN].scancodes.push_back((Uint8) stoi(ini.get_key_value("key bindings", "toggle_fullscreen")));
-}
+		actionmap[ACTIONS::TOGGLE_FULLSCREEN].scancodes.push_back((Uint8)stoi(ini.get_key_value("key bindings", "toggle_fullscreen")));
+	}
 
-void mgc::Keyboard::process_sdl_event(SDL_Event& event) {
-}
+	void Keyboard::process_sdl_event(SDL_Event& event) {
+	}
 
-void mgc::Keyboard::update() {
-	auto keyboard_state = SDL_GetKeyboardState(nullptr);
+	void Keyboard::update() {
+		auto keyboard_state = SDL_GetKeyboardState(nullptr);
 
-	for (auto& iter : actionmap) {
-		for (auto& scan_iter : iter.second.scancodes) {
-			if (keyboard_state[scan_iter]) {
-				iter.second.set(true, !iter.second.down());
-				break;
+		for (auto& iter : actionmap) {
+			for (auto& scan_iter : iter.second.scancodes) {
+				if (keyboard_state[scan_iter]) {
+					iter.second.set(true, !iter.second.down());
+					break;
+				}
+				iter.second.set(false, false);
 			}
-			iter.second.set(false, false);
 		}
 	}
-}
 
-mgc::Keyboard::Input_Action& mgc::Keyboard::get_action(ACTIONS action) {
-	return actionmap[action];
+	const Keyboard::Input_Action& Keyboard::get_action(const ACTIONS& action) const {
+		return actionmap.at(action);
+	}
 }

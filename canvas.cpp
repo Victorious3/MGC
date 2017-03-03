058dbc3771f7714a2d9617185cd39452645508e5
Victorious3 <vic@nightfall.moe>
2017-03-03 20:24:26 +0100

    Fix constexpr by moving to header

--- canvas.cpp
+++ canvas.cpp
@@ -6,22 +6,6 @@
 
 namespace mgc {
 
-	constexpr Color color(Uint8 r, Uint8 g, Uint8 b) {
-		return Color { r, g, b, 255 };
-	}
-
-	constexpr Color color(Uint8 r, Uint8 g, Uint8 b, Uint8 a) {
-		return Color { r, g, b, a };
-	}
-
-	constexpr Color color(Uint32 rbg) {
-		return Color { (Uint8)(rbg >> 16), (Uint8)(rbg >> 8), (Uint8)rbg, 255};
-	}
-
-	constexpr Color color_a(Uint32 rbga) {
-		return Color { (Uint8)(rbga >> 24), (Uint8)(rbga >> 16), (Uint8)(rbga >> 8) };
-	}
-
 	void Canvas::draw_gl_texture(GLuint texture, int x, int y, int w, int h, Color color) const {
 		draw_gl_texture(texture, x, y, w, h, 0, 0, 1, 1, color);
 	}

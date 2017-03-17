3cc47c2198a733eb8adfb6fe4dcd1cea71b178be
Victorious3 <vic@nightfall.moe>
2017-03-17 17:24:36 +0100

    Update project file to automatically copy dll files into the output directory

--- src/Keyboard.cpp
+++ src/Keyboard.cpp
@@ -15,7 +15,7 @@ namespace mgc {
 	void Keyboard::read_config(const ini::IniFile& ini) {
 		actionmap[ACTIONS::TOGGLE_FULLSCREEN].scancodes.clear();
 
-		actionmap[ACTIONS::TOGGLE_FULLSCREEN].scancodes.push_back(ini.get_section("key bindings")->get<Uint>("toggle_fullscreen", SDLK_F11));
+		actionmap[ACTIONS::TOGGLE_FULLSCREEN].scancodes.push_back(ini.get_section("key bindings")->get<Uint>("toggle_fullscreen", SDL_SCANCODE_F11));
 	}
 
 	void Keyboard::process_sdl_event(SDL_Event& event) {
@@ -23,7 +23,7 @@ namespace mgc {
 
 	void Keyboard::update() {
 		const Uint8* keyboard_state = SDL_GetKeyboardState(nullptr);
-
+		
 		for (auto& iter : actionmap) {
 			for (auto& scan_iter : iter.second.scancodes) {
 				if (keyboard_state[scan_iter]) {
